67e518e278a15e35d56e793be5430c9912fa4669
kodaimaehata <kodaimaehata@gmail.com>
2018-09-06 00:03:47 +0900

    リフレッシュトークン取得、アクセストークン取得、REST APIコール処理を追加

--- freeePOC/src/client/SalesforceOAuthClient.java
+++ freeePOC/src/client/SalesforceOAuthClient.java
@@ -12,22 +12,113 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 public class SalesforceOAuthClient {
-
+	
+	URL url ;
+	HttpURLConnection con ;
+	
+	
+		
 	public String getRefreshToken(String code){
-		String s_url = "https://login.salesforce.com/services/oauth2/token?grant_type=authorization_code&client_id=3MVG9YDQS5WtC11rOAJI.dCrM__xjpK8l.vH8pTONv4ICS_0EvJjHali6tHrJS1YXglW1_qvJ9cbrQcj82JLi&client_secret=6665015134456256708&redirect_uri=https://localhost/RefreshTokenGetter&code=";
+//		String s_url = "https://login.salesforce.com/services/oauth2/token?grant_type=authorization_code&client_id=3MVG9YDQS5WtC11rOAJI.dCrM__xjpK8l.vH8pTONv4ICS_0EvJjHali6tHrJS1YXglW1_qvJ9cbrQcj82JLi&client_secret=6665015134456256708&redirect_uri=https://localhost/RefreshTokenGetter&code=";
+		String s_url = "https://login.salesforce.com/services/oauth2/token?grant_type=authorization_code&client_id=3MVG9YDQS5WtC11rOAJI.dCrM__xjpK8l.vH8pTONv4ICS_0EvJjHali6tHrJS1YXglW1_qvJ9cbrQcj82JLi&client_secret=6665015134456256708&redirect_uri=https://&code=";
 
 		s_url = s_url + code;
 
 		System.out.println("url = " + s_url);
+		
+		openPOSTConnection(s_url);
+		String json = sendRequest();
 
-		URL url;
-		HttpURLConnection con;
-		String json = "";
+		String refresh_token = "";
+		try {
+			JSONObject jsonObj = new JSONObject(json);
+			refresh_token = jsonObj.getString("refresh_token");
+			System.out.println("Refresh_token is " + refresh_token);
+		}catch(JSONException e) {
+			e.printStackTrace();
+		}
+
+		return refresh_token;
+
+	}
+	
+	public String getAccessToken(String refresh_token) {
+		
+		String s_url = "https://login.salesforce.com/services/oauth2/token?grant_type=refresh_token&client_id=3MVG9YDQS5WtC11rOAJI.dCrM__xjpK8l.vH8pTONv4ICS_0EvJjHali6tHrJS1YXglW1_qvJ9cbrQcj82JLi&refresh_token=";
+
+		s_url = s_url + refresh_token;
+		
+		System.out.println(s_url);
+		
+		openPOSTConnection(s_url);
+		String response = sendRequest();
+
+		
+		String accessToken = "";
+
+		try {
+			JSONObject jsonObj = new JSONObject(response);
+			accessToken = jsonObj.getString("access_token");
+			System.out.println("accessToken is " + accessToken);
+		}catch(JSONException e) {
+			e.printStackTrace();
+		}
+		
+		return accessToken;
+	}
+	
+	public String getAccounts(String access_token) {
+		String accountsJSON = "";
+		
+		String s_url = "https://ap4.salesforce.com/services/apexrest/Act/";
+
+		openGETConnection(s_url);
+		con.setRequestProperty("Authorization", " Bearer " + access_token);
+		
+		sendRequest();
+		
+		return accountsJSON;
+	}
+	
+	private void openPOSTConnection(String s_url) {
+		try {
+			url = new URL(s_url);
+			con = (HttpURLConnection)url.openConnection();
+			con.setRequestMethod("POST");
+		} catch (MalformedURLException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		}
+		
+	}
+	
+	private void openGETConnection(String s_url) {
 		try {
 			url = new URL(s_url);
 			con = (HttpURLConnection)url.openConnection();
 			con.setRequestMethod("GET");
-			con.setRequestProperty("Content-Type","text/json;charset=utf-8");
+		} catch (MalformedURLException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public String sendRequest() {
+//		URL url;
+//		HttpURLConnection con;
+		String json = "";
+		try {
+//			url = new URL(s_url);
+//			con = (HttpURLConnection)url.openConnection();
+//			con.setRequestMethod("POST");
+//			con.setRequestProperty("Content-Type","text/json;charset=utf-8");
 
 			con.connect();
 
@@ -50,8 +141,12 @@ public class SalesforceOAuthClient {
 				System.out.println("Response JSON is " + json);
 
 			}else {
-				System.out.println("Error" + status);
+				System.out.println("Error code : " + status);
+				System.out.println("Error message : " + con.getResponseMessage());
+				return "";
 			}
+			
+			
 
 
 		} catch (MalformedURLException e1) {
@@ -61,18 +156,9 @@ public class SalesforceOAuthClient {
 			// TODO 自動生成された catch ブロック
 			e.printStackTrace();
 		}
-
-		String refresh_token = "";
-		try {
-			JSONObject jsonObj = new JSONObject(json);
-			refresh_token = jsonObj.getString("refresh_token");
-			System.out.println("Refresh_token is " + refresh_token);
-		}catch(JSONException e) {
-			e.printStackTrace();
-		}
-
-		return refresh_token;
-
+		
+		
+		return json;
 
 
 	}
--- freeePOC/test/client/SalesforceOAuthClientTest.java
+++ freeePOC/test/client/SalesforceOAuthClientTest.java
@@ -8,7 +8,7 @@ public class SalesforceOAuthClientTest {
 	public void test() {
 		SalesforceOAuthClient cli = new SalesforceOAuthClient();
 		try{
-			String reflesh_token = cli.getRefreshToken("aPrxFYRxG6g7t8oBX6FqguMfdbgPH.qXMc78WqbfrltLkG4bcVhd1YyQdnL9E73R2afTZ7ChWA%3D%3D");
+			String reflesh_token = cli.getRefreshToken("aPrxFYRxG6g7t8oBX6FqguMfdYJGwBgO5kCavp7tmtaOYhuZ8e1q.9Y_9DeX_JVPOyfBR9jF3w%3D%3D");
 			System.out.println(reflesh_token);
 			assert(true);
 		}catch(Exception e){
@@ -16,6 +16,28 @@ public class SalesforceOAuthClientTest {
 			assert(false);
 		}
 
+	}
+	
+	@Test
+	public void getaccess_tokentest() {
+		String refresh_token = "5Aep861..zRMyCurAUqXuPX5uJN1Yk7ghc6h4Cv7m6IPMhhMvOivwnD7dLeOFes5eM6el.JJ8pgW8h3tTQ3ngrr";
+		
+		SalesforceOAuthClient cli = new SalesforceOAuthClient();
+		
+		String access_token = cli.getAccessToken(refresh_token) ;
+		
+		System.out.println(access_token);
+		
+	}
+	
+	@Test
+	public void getAccountsTest() {
+		String access_token = "00D6F000002Tjy4!AQgAQK9zwJVvIAe2jK0Gd1SAhMTh0GCfrNk7QKAoSqjmlXwR71qctjLAyH.3itQcVKbspfGOdgazIk7oSwWniS0wxmhUj0sB";
+		
+		SalesforceOAuthClient cli = new SalesforceOAuthClient();
+		String accounts = cli.getAccounts(access_token);
+		
+		System.out.println(accounts);
 	}
 
 }
